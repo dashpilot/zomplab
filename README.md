d2c0998f83a976417b42fb59091f857ef3788a73
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2021-11-28 19:07:12 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# zomplab
-blog for html/js experiments
+# plakboek
---
+++ build.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const { builtinModules } = require('module');
+const fse = require('fs-extra');
+const { title } = require('process');
+
+var data = {};
+
+// fetch data
+readFolder(data, 'blog');
+
+// remove drafts in production
+if(process.env.VERCEL){
+  data.blog = data.blog.filter(x => x.data.draft !== true);
+}
+
+if (!fs.existsSync('./public/article')) {
+  fs.mkdirSync('./public/article', 0744);
+}
+
+// console.log(data.blog);
+
+var head = fs.readFileSync('./src/tpl/header.html').toString();
+var foot = fs.readFileSync('./src/tpl/footer.html').toString();
+
+var body = ``;
+data.blog.forEach(function(myitem){
+
+body += `<a class="article-list" href="/article/${myitem.slug}">${readableTitle(myitem.slug)}</a>`;
+
+});
+
+var page = head + body + foot;
+
+fs.writeFileSync('./public/index.html', page);
+
+data.blog.forEach(function(myitem){
+
+  let src = './src/article/'+myitem.slug;
+  let dest = './public/article/'+myitem.slug;
+
+  // create article folders
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, 0744);
+    fs.mkdirSync(dest+'/img', 0744);
+  }
+
+  var head = fs.readFileSync('./src/tpl/header.html').toString();
+  var foot = fs.readFileSync('./src/tpl/footer.html').toString();
+  
+  var page = head + myitem.body + foot;
+
+  fs.writeFileSync(dest+'/index.html', page);
+     
+  // copy images
+  if (fs.existsSync(src+'/img/')) {
+  fse.copySync(src+'/img/', dest+'/img/');
+  }
+
+  if (fs.existsSync(src+'/large/')) {
+    fse.copySync(src+'/large/', dest+'/large/');
+  }
+ 
+
+})
+
+
+/* --- helpers --- */
+
+function readFolder(data, folder){
+  const myFolder = './src/article/';
+
+  data[folder] = [];
+  fs.readdirSync(myFolder).forEach(filename => {
+
+  if(filename!=='.DS_Store'){
+      var newItem = {}
+      newItem.body = fs.readFileSync('./src/article/'+filename+'/body.html').toString();
+      newItem.slug = filename;
+
+      // list the images
+      newItem.images = [];
+      if (fs.existsSync(myFolder+filename+'/img')) {
+      fs.readdirSync(myFolder+filename+'/img/').forEach(imgname => {
+        if(imgname!=='.DS_Store'){
+          newItem.images.push(imgname);
+
+        }
+      });
+      }
+
+      
+      data[folder].push(newItem);
+  }
+   
+   
+  
+  });
+  
+  data[folder] = [...data[folder]].reverse();
+  return data;
+}
+
+
+function readableTitle(slug){
+  let title = slug.replace('-', ' ');
+  return title;
+}
